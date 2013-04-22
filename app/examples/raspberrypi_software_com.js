8b8e1f8230a4053f819aafd83b49eeb302c7e1b5
supernelis <nelis@archiwise.com>
2013-04-22 10:42:03 +0200

    changing the dns name

--- app/examples/raspberrypi_software_com.js
+++ app/examples/raspberrypi_software_com.js
@@ -1,9 +1,9 @@
 const GameEngine = require('../lib/game_engine');
 
-var xmppSrv = 'raspberrypi.softwarewolves.com';
-var gameCoordinator = 'sww@raspberrypi.softwarewolves.com';
+var xmppSrv = 'pi.softwarewolves.org';
+var gameCoordinator = 'sww@pi.softwarewolves.org';
 var gameCoordinatorPw = 's0ftwarew0lf';
-var moderator = 'softwarewolf@raspberrypi.softwarewolves.com';
+var moderator = 'softwarewolf@pi.softwarewolves.org';
 var moderatorPw = 's0ftwarew0lf';
 
 new GameEngine(gameCoordinator, gameCoordinatorPw, moderator, moderatorPw, xmppSrv);
