be4ff01cf69d1f818d4b3d9de5a7b202443b8c22
supernelis <nelis@archiwise.com>
2013-06-22 15:53:43 +0200

    fix for new dns pi + updated description of package

--- app/examples/raspberrypi_software_com.js
+++ app/examples/raspberrypi_software_com.js
@@ -1,9 +1,9 @@
 const GameEngine = require('../lib/game_engine');
 
-var xmppSrv = 'pi.softwarewolves.org';
-var gameCoordinator = 'sww@pi.softwarewolves.org';
+var xmppSrv = 'pi.software.wolf';
+var gameCoordinator = 'sww@pi.software.wolf';
 var gameCoordinatorPw = 's0ftwarew0lf';
-var moderator = 'softwarewolf@pi.softwarewolves.org';
+var moderator = 'softwarewolf@pi.software.wolf';
 var moderatorPw = 's0ftwarew0lf';
 
 new GameEngine(gameCoordinator, gameCoordinatorPw, moderator, moderatorPw, xmppSrv);
