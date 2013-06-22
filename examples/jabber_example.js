1059a828310490a922879ae0e8a3110fdb6c0f60
yo <yo@johanpeeters.com>
2013-06-22 10:21:03 +0200

    deleted old directories

--- examples/jabber_example.js
+++
@@ -1,9 +0,0 @@
-const GameEngine = require('../lib/game_engine');
-
-var gameCoordinator = 'sww@jabber.org';
-var gameCoordinatorPw = 's0ftwarew0lf';
-var xmppSrv = 'jabber.org';
-var moderator = 'softwarewolf@jabber.org';
-var moderatorPw = 's0ftwarew0lf';
-
-new GameEngine(gameCoordinator, gameCoordinatorPw, moderator, moderatorPw, xmppSrv);
--- examples/local_example.js
+++
@@ -1,9 +0,0 @@
-const GameEngine = require('../lib/game_engine');
-
-var gameCoordinator = 'sww@10.0.1.3';
-var gameCoordinatorPw = 's0ftwarew0lf';
-var xmppSrv = '10.0.1.3';
-var moderator = 'softwarewolf@10.0.1.3';
-var moderatorPw = 's0ftwarew0lf';
-
-new GameEngine(gameCoordinator, gameCoordinatorPw, moderator, moderatorPw, xmppSrv);
